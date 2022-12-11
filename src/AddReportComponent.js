c5f2c098c653ef1ed56cfb9f631e2ce0e8749f5c
JB <jakob.baumgartner@hotmail.com>
2022-12-11 16:34:18 +0100

    poraba

--- src/AddReportComponent.js
+++ src/AddReportComponent.js
@@ -5,6 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './App.css'
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 
@@ -18,33 +19,91 @@ export default class AddReportComponent extends Component {
 
         super(props);
         this.state = {
-            newReportDate: {
-                day: d.getDate(),
-                month: d.getMonth(),
-                year: d.getFullYear()
+            newReport: {
+                Date: {
+                    day: d.getDate(),
+                    month: d.getMonth(),
+                    year: d.getFullYear()
+                },
+                Articles: {
+                    $4n5pxq24kriob12ogd: {
+                        name: 'Twix 50g',
+                        price: 0.8,
+                        number: 7
+                    },
+                    $4n5pxq24ksiob12ogl: {
+                        name: 'Bounty',
+                        price: 0.68,
+                        number: 4
+                    },
+                    $4n5pxq24krio242oab: {
+                        name: 'Radenska',
+                        price: 0.9,
+                        number: 2
+                    }
+                },
+                Sum: 10.12
             }
         }
     }
 
-
-
+    rowa() {
+        console.log("hii")
+    }
 
 
 
     render() {
 
+        // list every inputed article on the list
+        var listNewArticles = [];
+
+        for (const [key, value] of Object.entries(this.state.newReport.Articles)) {
+
+            listNewArticles.push(
+                <Row onClick={this.rowa} className="rounded newlistArticle" style={{ margin: '5px', padding: '5px' }}>
+
+                    <Col>{value.name}</Col>
+                    <Col>{value.price} €</Col>
+                    <Col>{value.number}</Col>
+                    <Col>{Math.round(value.price * value.number * 100) / 100} €</Col>
+
+                </Row>
+            )
+        }
+
+
         return (
 
             <Card className="text-center" id="newReportCard" style={{ margin: 'auto', width: '90%', maxWidth: '1250px' }}>
-                <Card.Header as="h4">Poraba {this.state.newReportDate.day}.{this.state.newReportDate.month+1}.{this.state.newReportDate.year}</Card.Header>
+                <Card.Header as="h4">Poraba {this.state.newReport.Date.day}.{this.state.newReport.Date.month + 1}.{this.state.newReport.Date.year}</Card.Header>
                 <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Container>
+                        <Row>
+                            <Col><b>Artikel</b></Col>
+                            <Col><b>Cena</b></Col>
+                            <Col><b>Število</b></Col>
+                            <Col><b>Skupaj</b></Col>
+                        </Row>
+                        {listNewArticles}
+                    </Container>
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                <Card.Footer >
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Button variant="success" onClick={this.props.opendialogAddArticle}>Dodaj</Button>
+                            </Col>
+
+                            <Col>
+                                <b>Skupaj: {this.state.newReport.Sum} € </b>
+                            </Col>
+                            <Col>
+                                <Button variant="success">Shrani</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card.Footer>
             </Card>
 
         )
--- src/Poraba.js
+++ src/Poraba.js
@@ -19,11 +19,31 @@ export default class Poraba extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { users: ['Jakob Baumgartner', 'Aljaž Blažič', 'Miha Ožbot', 'Miloš Antič'], selectedUser: 'Jakob Baumgarter', dialogAddChange: 1 }
+        this.state = { users: ['Jakob Baumgartner', 'Aljaž Blažič', 'Miha Ožbot', 'Miloš Antič'],
+                        storage: {
+                            $4n5pxq24kriob12ogd: {
+                                name: 'Twix 50g',
+                                price: 0.8,
+                                number: 20
+                            },
+                            $4n5pxq24ksiob12ogl: {
+                                name: 'Bounty',
+                                price: 0.68,
+                                number: 15
+                            },
+                            $4n5pxq24krio242oab: {
+                                name: 'Radenska',
+                                price: 0.9,
+                                number: 4
+                            }
+                        },
+                         selectedUser: 'Jakob Baumgarter', 
+                         dialogAddChange: 0, 
+                         dialogAddArticle: 1 }
 
         this.closeDialog = this.closeDialog.bind(this)
         this.opendialogAddChange = this.opendialogAddChange.bind(this)
-        // this.openDialogAdd = this.openDialogAdd.bind(this)
+        this.opendialogAddArticle = this.opendialogAddArticle.bind(this)
         // this.removeArticle = this.removeArticle.bind(this)
 
     }
@@ -32,9 +52,14 @@ export default class Poraba extends Component {
         this.setState({ dialogAddChange: 1 })
     }
 
-    
+    opendialogAddArticle() {
+        this.setState({ dialogAddArticle: 1 })
+        console.log("hts")
+    }
+
+
     closeDialog() {
-        this.setState({ dialogAddChange: 0 });
+        this.setState({ dialogAddChange: 0, dialogAddArticle: 0 });
     }
 
     render() {
@@ -46,26 +71,65 @@ export default class Poraba extends Component {
         var dialog;
 
         if (this.state.dialogAddChange) {
-           
-           dialog = <Modal show="true">
-            <Modal.Header closeButton onClick={this.closeDialog}>
-                <Modal.Title>Polni račun</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                   <InputGroup className="mb-3">
-                        <InputGroup.Text>Polni €</InputGroup.Text>
-                        <Form.Control />
-                    </InputGroup>
-    
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" >
-                    Dodaj
-                </Button>
-            </Modal.Footer>
-        </Modal>
+
+            dialog = <Modal show="true">
+                <Modal.Header closeButton onClick={this.closeDialog}>
+                    <Modal.Title>Polni račun</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Polni €</InputGroup.Text>
+                            <Form.Control />
+                        </InputGroup>
+
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" >
+                        Dodaj
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        }
+        else if (this.state.dialogAddArticle) {
+
+            var artikels = [];
+
+            for (const [key, value] of Object.entries(this.state.storage)) {
+                console.log(`${key}: ${value}`);
+                artikels.push(<option><p>{value.name}  ( {value.number} ) </p></option>)
+              }
+
+        
+
+            dialog = <Modal show="true">
+                <Modal.Header closeButton onClick={this.closeDialog}>
+                    <Modal.Title>Dodaj artikel</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Artikel: </InputGroup.Text>
+                            <Form.Select defaultValue="">
+                              {artikels}
+                            </Form.Select>
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Število: </InputGroup.Text>
+                            <Form.Control />
+                        </InputGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success">
+                        Dodaj
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
+
         }
         else {
             dialog = '';
@@ -79,18 +143,18 @@ export default class Poraba extends Component {
 
                 {dialog}
 
-    <ListGroup horizontal id="stanje">
+                <ListGroup horizontal id="stanje">
                     <ListGroup.Item><h5>Stanje</h5><br />15eur</ListGroup.Item>
                 </ListGroup>
 
-                <Button variant="primary" id="polni" onClick={this.opendialogAddChange}>Polni</Button>
+                <Button variant="warning" id="polni" onClick={this.opendialogAddChange}>Polni</Button>
 
-                <DropdownButton id="dropdown-names-button" title={this.state.selectedUser}  >
+                <DropdownButton variant="warning" id="dropdown-names-button" title={this.state.selectedUser}  >
                     {listNames}
                 </DropdownButton>
-            
 
-                <AddReportComponent />
+
+                <AddReportComponent opendialogAddArticle={this.opendialogAddArticle} />
 
             </div>
         )
