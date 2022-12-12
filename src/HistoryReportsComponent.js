9a1863b8adc74e240d1bfa8bec51e89976bbe75d
JB <jakob.baumgartner@hotmail.com>
2022-12-12 16:25:49 +0100

    history component upgrade

--- src/HistoryReportsComponent.js
+++ src/HistoryReportsComponent.js
@@ -4,63 +4,184 @@ import './index.css';
 import Accordion from 'react-bootstrap/Accordion';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 
 
 
 
 export default class HistoryReportsComponent extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {bonus: 0, boughtPrice: this.props.boughtPrice}
-        this.changeSlider = this.changeSlider.bind(this)
-    }
 
-    changeSlider (event) {
-
-        console.log(event.target.value)
-        // var newBonus = event.target.value 
-        this.setState({bonus: Math.round(event.target.value * this.props.boughtPrice) / 100,
-                        boughtPrice: Math.round((Number(this.props.boughtPrice) + this.state.bonus)*100)/100})
-        
-    }
-
-    
-
-    render() {  
-
-        return(
-            
-            <Accordion id="historyReport">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Accordion Item #1</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Accordion Item #2</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-
-        )
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      reports: {
+        $6854jgfh: {
+          Sort: 'articles',
+          Date: {
+            day: 12,
+            month: 3,
+            year: 2022
+          },
+          Articles: {
+            $4n5pxq24kriob12ogd: {
+              name: 'Twix 50g',
+              price: 0.8,
+              number: 7
+            },
+            $4n5pxq24ksiob12ogl: {
+              name: 'Bounty',
+              price: 0.68,
+              number: 4
+            },
+            $4n5pxq24krio242oab: {
+              name: 'Radenska',
+              price: 0.9,
+              number: 2
+            }
+          },
+          Sum: 10.12
+        },
+        $6854jfdf:{
+          Sort: 'money',
+          Date: {
+            day: 8,
+            month: 4,
+            year: 2022
+          },
+          Amount: 25
+        },
+        $68gfjgfh: {
+          Sort: 'articles',
+          Date: {
+            day: 8,
+            month: 4,
+            year: 2022
+          },
+          Articles: {
+            $4n5pxq24kriob12ogd: {
+              name: 'Twix 50g',
+              price: 0.8,
+              number: 7
+            },
+            $4n5pxq24ksiob12ogl: {
+              name: 'Bounty',
+              price: 0.68,
+              number: 4
+            },
+            $4n5pxq24krio242oab: {
+              name: 'Radenska',
+              price: 0.9,
+              number: 2
+            }
+          },
+          Sum: 10.12
+        },
+        $685ajfdf:{
+          Sort: 'bonus',
+          Date: {
+            day: 1,
+            month: 1,
+            year: 2022
+          },
+          Amount: 13.4
+        },
+      }
     }
   }
+
+
+
+  render() {
+
+    // list every inputed article on the list
+    var HistoryReports = [];
+    var listNewArticles = [];
+
+    for (const [key, value] of Object.entries(this.state.reports)) {
+
+      if (value.Sort == 'articles') {
+
+      listNewArticles = [];
+
+      console.log(value.Articles)
+
+      for (const [k, v] of Object.entries(value.Articles)) {
+   
+
+        listNewArticles.push(
+            <Row className="rounded" style={{ margin: '5px', padding: '5px' }}>
+
+                <Col>{v.name}</Col>
+                <Col>{v.price} €</Col>
+                <Col>{v.number}</Col>
+                <Col>{Math.round(v.price * v.number * 100) / 100} €</Col>
+            </Row>
+        )
+    }
+
+      HistoryReports.push(
+        <Accordion.Item eventKey={key}>
+          <Accordion.Header><h5>{value.Date.day}.{value.Date.month + 1}.{value.Date.year}</h5></Accordion.Header>
+          <Accordion.Body>
+          <Container>
+                        <Row>
+                            <Col><b>Artikel</b></Col>
+                            <Col><b>Cena</b></Col>
+                            <Col><b>Število</b></Col>
+                            <Col><b>Skupaj</b></Col>
+                        </Row>
+                        {listNewArticles}
+                        <Row>
+                          <Col><b>Skupaj: {value.Sum} €</b></Col>
+                        </Row>
+                    </Container>
+          </Accordion.Body>
+        </Accordion.Item>
+      )
+      }
+
+      if (value.Sort == 'money') {
+        HistoryReports.push(
+          <Accordion.Item eventKey={key}>
+            <Accordion.Header><h5>{value.Date.day}.{value.Date.month + 1}.{value.Date.year}</h5></Accordion.Header>
+            <Accordion.Body>
+            <Container>
+                          <Row>
+                            <Col><b>Polnitev: {value.Amount} €</b></Col>
+                          </Row>
+                      </Container>
+            </Accordion.Body>
+          </Accordion.Item>)
+    }
+
+    if (value.Sort == 'bonus') {
+      HistoryReports.push(
+        <Accordion.Item eventKey={key}>
+          <Accordion.Header><h5>{value.Date.day}.{value.Date.month + 1}.{value.Date.year} </h5></Accordion.Header>
+          <Accordion.Body>
+          <Container>
+                        <Row>
+                          <Col><b>Pripis: {value.Amount} €</b></Col>
+                        </Row>
+                    </Container>
+          </Accordion.Body>
+        </Accordion.Item>)
+  }
+
+  }
+  
+
+    return (
+
+      <Accordion id="historyReport">
+        {HistoryReports}
+
+      </Accordion>
+
+    )
+
+  }
+}
 
